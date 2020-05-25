e38d8fedf155d3569d249728b60f6c7873beee40
Cornelis Heuperman <cheuperman@gmail.com>
2020-05-25 09:53:47 +0100

    Pass env variabes to pipeline

--- src/Firebase.tsx
+++ src/Firebase.tsx
@@ -4,10 +4,10 @@ import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  authDomain: "free-from-finder.firebaseapp.com",
+  databaseURL: "https://free-from-finder.firebaseio.com",
+  projectId: "free-from-finder",
+  storageBucket: "free-from-finder.appspot.com",
   messagingSenderId: process.env.FIREBASE_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
   measurementId: process.env.FIREBASE_MEASUREMENT_ID
