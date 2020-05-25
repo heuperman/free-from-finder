5cfd6e14b7f0a2f4367b4ca55e91d2aead01b5f0
Cornelis Heuperman <cheuperman@gmail.com>
2020-05-25 11:49:43 +0100

    Switch deployment to Firebase

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,5 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "production",
-  output: merge(common.output, {
-    publicPath: "/free-from-finder/"
-  })
+  mode: "production"
 });
